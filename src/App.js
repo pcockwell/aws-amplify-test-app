740d570946761208804108deaa17ad0e928a61d4
Patrick Cockwell <pcockwell@gmail.com>
2021-03-07 21:24:38 +0700

    Add styling for login

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import logo from './logo.svg';
 import './App.css';
 import { Auth } from 'aws-amplify'
 import { withAuthenticator } from 'aws-amplify-react'
+import '@aws-amplify/ui/dist/style.css';
 
 function App() {
   return (
