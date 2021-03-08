83e0910d5925adca8d6d714b1040859309b3a203
Patrick Cockwell <pcockwell@gmail.com>
2021-03-08 14:38:46 +0700

    Add allow origin cors header

--- amplify/backend/function/amplifyTestLamdbaFn/src/index.py
+++ amplify/backend/function/amplifyTestLamdbaFn/src/index.py
@@ -5,8 +5,8 @@ def handler(event, context):
   return {
       'statusCode': 200,
       'headers': {
-          'Access-Control-Allow-Headers': '*',
-          'Access-Control-Allow-Origin': '*',
+          'Access-Control-Allow-Headers': 'Content-Type',
+          'Access-Control-Allow-Origin': 'https://main.dx0x4jcfvd33i.amplifyapp.com/',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PUT'
       },
       'body': "Hello from your new Amplify Python lambda!"
