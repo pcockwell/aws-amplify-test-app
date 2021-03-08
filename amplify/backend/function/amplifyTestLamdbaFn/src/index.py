329791f285bcb5ae5a4355a193054fd025041312
Patrick Cockwell <pcockwell@gmail.com>
2021-03-08 21:35:15 +0700

    Update response

--- amplify/backend/function/amplifyTestLamdbaFn/src/index.py
+++ amplify/backend/function/amplifyTestLamdbaFn/src/index.py
@@ -1,10 +1,63 @@
+import os
+
+import boto3
+
+def get_s3_presigned_post_url(org, filename, expiry=3600):
+  """
+  Method to get post url from s3
+  :param key: key of the file
+  :return:
+  """
+  key = '/'.join([org, filename])
+  try:
+    presigned_post = boto3.client('s3').generate_presigned_post(
+        os.environ['STORAGE_AMPLIFYTESTS3_BUCKETNAME'],
+        key,
+        ExpiresIn=expiry
+    )
+    return {
+      'success': 'true',
+      'data': {
+        'upload_url': presigned_post['url'],
+        'fields': presigned_post['fields']
+      }
+    }
+  except Exception as e:
+    return {
+      'success': 'false',
+      'error': 'Unable to upload PDF'
+    }
+
+
+def get_user_organization(identity):
+  return 'test-org'
+  client = boto3.client('cognito-idp')
+  print(identity)
+  user = client.get_user(AccessToken=identity['accessKey'])
+  organization = None
+  for attribute in user['UserAttributes']:
+    if attribute['Name'] == 'custom:organization':
+      organization = attribute['Value']
+      break
+  print(user)
+  print(organization)
+  return organization
+
+
 def handler(event, context):
-  return {
-      'statusCode': 200,
-      'headers': {
-          'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Allow-Origin': 'https://main.dx0x4jcfvd33i.amplifyapp.com',
-          'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
-      },
-      'body': "Hello from your new Amplify Python lambda!"
+  body = event['queryStringParameters']
+  organization = get_user_organization(event['requestContext']['identity'])
+  presigned_url_details = get_s3_presigned_post_url(organization, body['name'])
+  print('handler')
+  print(presigned_url_details)
+  response = {
+    'statusCode': 200,
+    'headers': {
+        'Access-Control-Allow-Headers': 'Content-Type',
+        'Access-Control-Allow-Origin': 'https://main.dx0x4jcfvd33i.amplifyapp.com',
+        'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
+    },
+    'body': presigned_url_details
   }
+  print(response)
+  return response
