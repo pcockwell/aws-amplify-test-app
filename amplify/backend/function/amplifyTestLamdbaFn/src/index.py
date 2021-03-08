67000c9d5bbce50d072ea483a74c5f0dde3e6d28
Patrick Cockwell <pcockwell@gmail.com>
2021-03-08 21:08:20 +0700

    Go back a bit to verify cors is working

--- amplify/backend/function/amplifyTestLamdbaFn/src/index.py
+++ amplify/backend/function/amplifyTestLamdbaFn/src/index.py
@@ -1,56 +1,4 @@
-import os
-
-import boto3
-
-def get_s3_presigned_post_url(org, filename, expiry=3600):
-    """
-    Method to get post url from s3
-    :param key: key of the file
-    :return:
-    """
-    key = '/'.join([org, filename])
-    print(key)
-      presigned_post = boto3.client('s3').generate_presigned_post(
-          os.environ['STORAGE_AMPLIFYTESTS3_BUCKETNAME'],
-          key,
-          ExpiresIn=expiry
-      )
-      return {
-        'success': True,
-        'data': {
-          'upload_url': presigned_post['url'],
-          'fields': presigned_post['fields']
-          }
-        }
-    except Exception as e:
-        print(e)
-        return {
-          'success': False,
-          'error': 'Unable to upload PDF'
-        }
-
-
-def get_user_organization(identity):
-  return 'test-org'
-  client = boto3.client('cognito-idp')
-  print(identity)
-  user = client.get_user(AccessToken=identity['accessKey'])
-  organization = None
-  for attribute in user['UserAttributes']:
-    if attribute['Name'] == 'custom:organization':
-      organization = attribute['Value']
-      break
-  print(user)
-  print(organization)
-  return organization
-
-
 def handler(event, context):
-  print(event)
-  body = event['queryStringParameters']
-  organization = get_user_organization(event['requestContext']['identity'])
-  presigned_url_details = get_s3_presigned_post_url(organization, body['name'])
-  print(presigned_url_details)
   return {
       'statusCode': 200,
       'headers': {
@@ -58,5 +6,5 @@ def handler(event, context):
           'Access-Control-Allow-Origin': 'https://main.dx0x4jcfvd33i.amplifyapp.com',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET'
       },
-      'body': presigned_url_details
+      'body': "Hello from your new Amplify Python lambda!"
   }
