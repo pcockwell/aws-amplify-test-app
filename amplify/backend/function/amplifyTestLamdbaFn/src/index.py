03854c25daaac4203c36e0136cdeb9c72aac0dad
Patrick Cockwell <pcockwell@gmail.com>
2021-03-08 15:02:03 +0700

    Fix allow origin

--- amplify/backend/function/amplifyTestLamdbaFn/src/index.py
+++ amplify/backend/function/amplifyTestLamdbaFn/src/index.py
@@ -6,7 +6,7 @@ def handler(event, context):
       'statusCode': 200,
       'headers': {
           'Access-Control-Allow-Headers': 'Content-Type',
-          'Access-Control-Allow-Origin': 'https://main.dx0x4jcfvd33i.amplifyapp.com/',
+          'Access-Control-Allow-Origin': 'https://main.dx0x4jcfvd33i.amplifyapp.com',
           'Access-Control-Allow-Methods': 'OPTIONS,POST,GET,PUT'
       },
       'body': "Hello from your new Amplify Python lambda!"
